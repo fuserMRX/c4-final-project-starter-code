3c0de9da025cef5c111e360d3b081085d471c89e
fuserMRX <vnovodid@gmail.com>
2022-12-17 17:26:36 +0200

    feat: re-factor getTodos and add a backend

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,8 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example: - DONE
-  // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-imwin-7q.us.auth0.com',            // Auth0 domain
   clientId: 'wn1MOihOvdXZLhgNvXrz2XzFoRMPbCVj',          // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://localhost:3000/callback',
 }
