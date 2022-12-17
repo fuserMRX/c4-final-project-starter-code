ca0f64a413c4f514746da8413103a778de1db19b
fuserMRX <vnovodid@gmail.com>
2022-12-17 17:46:57 +0200

    feat: add createTodo.js lambda function implementation

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,16 +1,14 @@
-require('source-map-support/register');
-const express = require('express');
-const serverlessExpress = require('@vendia/serverless-express');
+import 'source-map-support/register';
 
-// import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import { createLogger } from '../../utils/logger';
-// import * as middy from 'middy'
-// import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest';
 import { getUserId } from '../utils';
 import { createTodo } from '../../businessLogic/todos';
 
+const express = require('express');
+const serverlessExpress = require('@vendia/serverless-express');
 const app = express();
+
 const logger = createLogger('createTodo');
 
 
@@ -31,21 +29,4 @@ app.post('/todos', async (req, res) => {
 
 });
 
-exports.handler = serverlessExpress({app});
-
-
-
-
-// export const handler = middy(
-//   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//     const newTodo: CreateTodoRequest = JSON.parse(event.body)
-//     // TODO: Implement creating a new TODO item
-
-//     return undefined
-// )
-
-// handler.use(
-//   cors({
-//     credentials: true
-//   })
-// )
+exports.handler = serverlessExpress({ app });
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -2,7 +2,6 @@ import 'source-map-support/register';
 
 const express = require('express');
 const serverlessExpress = require('@vendia/serverless-express');
-const { getCurrentInvoke } = require('@vendia/serverless-express');
 const app = express();
 
 import { createLogger } from '../../utils/logger';
@@ -12,7 +11,7 @@ import { getUserId } from '../utils';
 const logger = createLogger('getTodos');
 
 app.get('/todos', async (req, res) => {
-  const { event, context } = getCurrentInvoke();
+  const { event, context } = serverlessExpress.getCurrentInvoke();
 
   logger.info(`GetTodos info about event ==> ${JSON.stringify(event)}`);
   logger.info(`GetTodos info about context ==> ${JSON.stringify(context)}`);
